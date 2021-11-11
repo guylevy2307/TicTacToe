391a08735c62266e1f115f5e38619eec703caf4f
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2021-11-11 10:52:34 +0200

    adding reset action that run if its tie

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int turn=0;
-
+int[][] mat=new int[3][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,28 +28,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
           Button bt=(Button) this.findViewById(resID);
             bt.setOnClickListener(this);
         }
-        TextView tv=(TextView) this.findViewById(R.id.main_winnnig);
-                tv.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if(turn==0){
-                            tv.setText("O start,Good luck");
-                        }
-                            else if(turn%2==0){
-                                tv.setText("It's O turn now!");
-                        }else{
-                            tv.setText("It's X turn now!");
-                        }
-                    }
-                });
-
     }
 
-public void checkWin(){
+        public boolean checkWin(){
 
 
-
-}
+                return false;
+        }
+        public void resetGame(){
+            String text="button";
+            for(int i=1;i<10;i++){
+                String bText=text.concat(String.valueOf(i));
+                //Log.d("TAG", "onCreate: "+bText);
+                int resID = getResources().getIdentifier(bText, "id",getPackageName());
+                //Log.d("TAG", "Find: "+resID);
+                Button bt=(Button) this.findViewById(resID);
+                bt.setText("Click");
+                bt.setBackgroundResource(0);
+                turn=0;
+            }
+        }
 
     @Override
     public void onClick(View v) {
@@ -57,6 +56,7 @@ public void checkWin(){
             Toast.makeText(getApplicationContext(),"Please select another box this one is taken!",Toast.LENGTH_LONG).show();
         }
        else {
+            TextView tv=(TextView) this.findViewById(R.id.main_winnnig);
             switch (b.getId()) {
                 case R.id.button1:
                     // do something
@@ -87,19 +87,36 @@ public void checkWin(){
                     break;
 
             }
-            if (turn % 2 == 0) {
+            if (turn % 2 == 0)//now "0" play
+                {
                 b.setBackgroundResource(R.drawable.o);
-                //tv.setText("X turn");
+               tv.setText("X turn");
 
-            } else {
+            } else //turn%0==1 now "x" play
+                {
                 b.setBackgroundResource(R.drawable.x);
-                // tv.setText("O turn");
+                 tv.setText("O turn");
             }
             turn++;
             b.setText("");
 
             if (turn > 3) {
-                checkWin();
+               boolean win= checkWin();
+               if(win){
+
+                   resetGame();
+               }
+               if(turn==9){
+                   Toast.makeText(getApplicationContext(),"Its tie!",Toast.LENGTH_LONG).show();
+
+                   new android.os.Handler(Looper.getMainLooper()).postDelayed(
+                           new Runnable() {
+                               public void run() {
+                                   resetGame();
+                               }
+                           },
+                           2000);
+               }
             }
         }
     }
